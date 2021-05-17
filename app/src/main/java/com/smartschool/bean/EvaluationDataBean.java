6b36d8ee059b43592cbcaede6d6f90fb81280827
UR-Amedeus <805825745@qq.com>
2021-05-17 21:46:55 +0800

    msg

---
+++ app/src/main/java/com/smartschool/bean/EvaluationDataBean.java
@@ -0,0 +1,31 @@
+package com.smartschool.bean;
+
+public class EvaluationDataBean {
+    String courseName;
+    String type;
+    String teacher;
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+}
---
+++ app/src/main/java/com/smartschool/ui/dashboard/EvaluationActivity.java
@@ -0,0 +1,16 @@
+package com.smartschool.ui.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.smartschool.R;
+
+public class EvaluationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluation);
+    }
+}

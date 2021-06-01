5cc8be24daee5964158936cf4088bd6347a5b144
UR-Amedeus <805825745@qq.com>
2021-06-01 08:37:59 +0800

    msg

---
+++ app/src/main/java/com/smartschool/ui/home/InfoActivity.java
@@ -0,0 +1,14 @@
+package com.smartschool.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
